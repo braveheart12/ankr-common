45d68dd9a03605d087fc9fabe61960f235fe795f
Tony Wu <xiaowu@berkeley.edu>
2019-05-17 11:18:36 -0700

    [SWDEV-247]UPDATEDcmgr
    [SWDEV-247]Update dcmgr

--- protos/monitor/account/v1/fetchaccounts.micro.go
+++ protos/monitor/account/v1/fetchaccounts.micro.go
@@ -3,9 +3,11 @@
 
 package account
 
-import proto "github.com/golang/protobuf/proto"
-import fmt "fmt"
-import math "math"
+import (
+	fmt "fmt"
+	proto "github.com/golang/protobuf/proto"
+	math "math"
+)
 
 import (
 	context "context"
@@ -17,6 +19,12 @@ import (
 var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the proto package it is being compiled against.
+// A compilation error at this line likely means your copy of the
+// proto package needs to be updated.
+const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
