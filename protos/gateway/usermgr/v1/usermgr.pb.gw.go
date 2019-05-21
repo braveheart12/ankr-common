57dd9ccd56e1ad4b73aecb6f508ccbb7e94c86f8
Tony Wu <xiaowu@berkeley.edu>
2019-05-21 16:03:12 -0700

    [SWDEV-247]Update provider api for outside proto

--- protos/gateway/usermgr/v1/usermgr.pb.gw.go
+++ protos/gateway/usermgr/v1/usermgr.pb.gw.go
@@ -268,6 +268,23 @@ func request_UserMgr_UserDetail_0(ctx context.Context, marshaler runtime.Marshal
 
 }
 
+func request_UserMgr_ApplyBecomeClusterProvider_0(ctx context.Context, marshaler runtime.Marshaler, client UserMgrClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ClusterProviderApplyRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.ApplyBecomeClusterProvider(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterUserMgrHandlerFromEndpoint is same as RegisterUserMgrHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterUserMgrHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -606,6 +623,26 @@ func RegisterUserMgrHandlerClient(ctx context.Context, mux *runtime.ServeMux, cl
 
 	})
 
+	mux.Handle("POST", pattern_UserMgr_ApplyBecomeClusterProvider_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_UserMgr_ApplyBecomeClusterProvider_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_UserMgr_ApplyBecomeClusterProvider_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -639,6 +676,8 @@ var (
 	pattern_UserMgr_SearchDeposit_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"deposit", "search"}, ""))
 
 	pattern_UserMgr_UserDetail_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"user", "detail"}, ""))
+
+	pattern_UserMgr_ApplyBecomeClusterProvider_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"user", "applyprovider"}, ""))
 )
 
 var (
@@ -671,4 +710,6 @@ var (
 	forward_UserMgr_SearchDeposit_0 = runtime.ForwardResponseMessage
 
 	forward_UserMgr_UserDetail_0 = runtime.ForwardResponseMessage
+
+	forward_UserMgr_ApplyBecomeClusterProvider_0 = runtime.ForwardResponseMessage
 )
